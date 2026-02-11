2f78bdf6496af728acb3caf52a805d7eb9052d8e
Darsik M <www.bla-bla-bla@bk.ru>
2026-02-11 16:09:51 +0300

    Правки после ревью

--- task_1.py
+++ task_1.py
@@ -29,14 +29,8 @@ class ExtendedCase(Case):
         self.environment = environment
 
     def print_test_case_info(self):
-        print(
-            f"ID тест-кейса:  {self.test_case_id}"
-            f"\nНазвание: {self.name}"
-            f"\nОписание шага: {self.step_description}"
-            f"\nОжидаемый результат: {self.expected_result}"
-            f"\nПредусловие: {self.precondition}"
-            f"\nОкружение: {self.environment}"
-        )
+        Case.print_test_case_info(self)
+        print(f"Предусловие: {self.precondition}" f"\nОкружение: {self.environment}")
 
 
 case = ExtendedCase(
